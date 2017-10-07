f11f6f28bd4f29929fd6d850cf974315f5e33d5c
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-10-07 18:56:49 +0100

    bottom hours array starts with four

--- BerlinClockTest.java
+++ BerlinClockTest.java
@@ -36,13 +36,13 @@ public class BerlinClockTest {
     }
 //
 //     // Bottom hours should light a red lamp for every hour left from top hours
-//     @Test
-//     public void testBottomHoursShouldLightRedLampForEveryHourLeftFromTopHours() {
-//         Assert.assertEquals("OOOO", berlinClock.getBottomHours(0));
-//         Assert.assertEquals("RRRO", berlinClock.getBottomHours(13));
-//         Assert.assertEquals("RRRO", berlinClock.getBottomHours(23));
-//         Assert.assertEquals("RRRR", berlinClock.getBottomHours(24));
-//     }
+    @Test
+    public void testBottomHoursShouldLightRedLampForEveryHourLeftFromTopHours() {
+        Assert.assertEquals("OOOO", berlinClock.getBottomHours(0));
+        // Assert.assertEquals("RRRO", berlinClock.getBottomHours(13));
+        // Assert.assertEquals("RRRO", berlinClock.getBottomHours(23));
+        // Assert.assertEquals("RRRR", berlinClock.getBottomHours(24));
+    }
 //
 //     // Top minutes should have 11 lamps
 //     @Test
